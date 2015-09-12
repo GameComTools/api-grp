a6e7ad3f7c99595d56d29be091a02b46ee27627e
Kody Peterson <Kody.Peterson@disney.com>
2015-09-12 14:38:37 -0400

    deployment support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# Install dependencies
 apt-get install sshpass
+
+# Connect and Deploy
 sshpass -e ssh root@45.55.208.46  -o StrictHostKeyChecking=no "echo 'hello'"
